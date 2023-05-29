dc1985ff4ad4e6c5990542d0761d6a5b741cc3d5
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2023-05-30 01:08:54 +0300

    khod ya yusef tani

--- src/components/FoodCourt.jsx
+++ src/components/FoodCourt.jsx
@@ -1,32 +1,120 @@
-import React from "react";
+import React from 'react'
+import './FoodCourt.css'
+import pexel from '../data/pexels-chan-walrus-958545.jpg'
+import restaurant from '../data/restaurant.png'
+import search from '../data/search-interface-symbol.png'
+
 
 const FoodCourt = () => {
   return (
-       
-       <div class="">
-      <div className="grid place-items-center h-[500px] my-[100px] ">
-        <div class="">
-            <a className="w-64 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-[#ff711f] hover:border[#ff711f] hover:bg-[#ff711f] hover:text-white shadow-md py-2 px-6 items-center text-center" href="/ckmkitchens">Kitchen One</a>
+    <div>
+
+<div class="topnav">
+        <p>CKM
+        <h1>food court</h1>
+        </p>
+        <div><img src={search} class="img1" width="10px" height="10px" alt=""/><input type="search"
+                name="" id=""/></div>
+    </div>
+    <div>
+        <h1>TRUSTED BY 120 restaurant</h1>
+    </div>
+    <div class="all_section">
+        <div class="contaner1">
+            <div class="img_container"> 
+                <img src={restaurant} class="img3" alt=""/>
+                {/* <img src={pexel} class="img2"
+                width="100%" height="100%" alt=""/> */}
+            </div>
+            <div class="discription" id="discription1">
+                <div>
+                    <p>kitchen name</p>
+                    <p>kitchen description</p>
+                    <p>notes</p>
+                </div>
+            </div>
+        </div><br/>
+        <div class="contaner1">
+            <div class="img_container">
+                <img src={restaurant} class="img3" alt=""/>
+                {/* <img src={pexel} class="img2"
+                width="100%" height="100%" alt=""/> */}
+            </div>
+            <div class="discription" id="discription2">
+                <div>
+                    <p>kitchen name</p>
+                    <p>kitchen description</p>
+                    <p>notes</p>
+                </div>
+            </div>
+        </div><br/>
+        <div class="contaner1">
+            <div class="img_container">
+                <img src={restaurant} class="img3" alt=""/>
+                {/* <img src={pexel} class="img2"
+                width="100%" height="100%" alt=""/> */}
+            </div>
+            <div class="discription" id="discription3">
+                <div>
+                    <p>kitchen name</p>
+                    <p>kitchen description</p>
+                    <p>notes</p>
+                </div>
+            </div>
+        </div><br/>
+        <div class="contaner1">
+            <div class="img_container">
+                <img src={restaurant} class="img3" alt=""/>
+                {/* <img src={pexel} class="img2"
+                width="100%" height="100%" alt=""/> */}
+            </div>
+            <div class="discription" id="discription4">
+                <div>
+                    <p>kitchen name</p>
+                    <p>kitchen description</p>
+                    <p>notes</p>
+                </div>
+            </div>
+        </div><br/>
+        <div class="contaner1">
+            <div class="img_container">
+                <img src={restaurant} class="img3" alt=""/>
+                {/* <img src={pexel} class="img2"
+                width="100%" height="100%" alt=""/> */}
+            </div>
+            <div class="discription" id="discription5">
+                <div>
+                    <p>kitchen name</p>
+                    <p>kitchen description</p>
+                    <p>notes</p>
+                </div>
+            </div>
+        </div><br/>
+        <div class="contaner1">
+            <div class="img_container">
+                <img src={restaurant} class="img3" alt=""/>
+                {/* <img src={pexel} class="img2"
+                width="100%" height="100%" alt=""/> */}
+            </div>
+            <div class="discription" id="discription6">
+                <div>
+                    <p>kitchen name</p>
+                    <p>kitchen description</p>
+                    <p>notes</p>
+                </div>
+            </div>
+        </div><br/>
+        </div><br/>
+        <div class="slider">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>...</p>
         </div>
 
-        <div class="">
-            <a className="w-64 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-[#ff711f] hover:border[#ff711f] hover:bg-[#ff711f] hover:text-white shadow-md py-2 px-6 items-center text-center" href="/ckmkitchens">Kitchen Two</a>
-        </div>
+    </div>
+  )
+}
 
-        <div class="">
-            <a className="w-64 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-[#ff711f] hover:border[#ff711f] hover:bg-[#ff711f] hover:text-white shadow-md py-2 px-6 items-center text-center" href="/ckmkitchens">Kitchen Three</a>
-        </div>
-
-        <div class="">
-            <a className="w-64 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-[#ff711f] hover:border[#ff711f] hover:bg-[#ff711f] hover:text-white shadow-md py-2 px-6 items-center text-center" href="/ckmkitchens">Kitchen Four</a>
-        </div>
-
-        <div class="">
-            <a className="w-64 bg-white tracking-wide text-gray-800 font-bold rounded border-b-2 border-[#ff711f] hover:border[#ff711f] hover:bg-[#ff711f] hover:text-white shadow-md py-2 px-6 items-center text-center" href="/ckmkitchens">Kitchen Five</a>
-        </div>
-</div>
-      </div>
-  );
-};
-
-export default FoodCourt;
+export default FoodCourt
