7632ce209082a933c5744be095968fd092520c70
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2023-06-18 11:03:13 +0300

    adminlte

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,138 +1,340 @@
 import React from "react";
-import { BsCurrencyDollar } from "react-icons/bs";
-import { GoPrimitiveDot } from "react-icons/go";
-import { IoIosMore } from "react-icons/io";
-import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
-
-import { Stacked, Pie, Button, LineChart, SparkLine } from "../components";
-import PieChart from "../components/pieChart";
-import {
-  earningData,
-  medicalproBranding,
-  recentTransactions,
-  weeklyStats,
-  dropdownData,
-  SparklineAreaData,
-  ecomPieChartData,
-} from "../data/dummy";
-import { useStateContext } from "../contexts/ContextProvider";
-import product9 from "../data/product9.jpg";
-import {
-  Chart as ChartJS,
-  ArcElement,
-  Tooltip,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
-import { Doughnut } from "react-chartjs-2";
-
-ChartJS.register(
-  ArcElement,
-  Tooltip,
-  Legend,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title
-);
-
+import { logo } from "../data/avatar2.jpg";
 
 const Analytics = () => {
-  const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-  const options = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: "",
-      },
-    },
-  };
-  const labels = [
-    "jan",
-    "feb",
-    "mar",
-    "apr",
-    "may",
-    "jun",
-    "jul",
-    "aug",
-    "sep",
-    "oct",
-    "nov",
-    "dec",
-  ];
-  const labels1 = [
-    "jan",
-    "feb",
-    "mar",
-    "apr",
-    "may",
-  
-  ];
-  const data1 = {
-    labels,
-    datasets: [
-      {
-        label: "Dataset 1",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 100000 })),
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-      
-    ],
-  };
-  const data2 = {
-    labels1,
-    datasets: [
-      {
-        label: "Staff",
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 10000 })),
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-    ],
-  };
   return (
-    <div className=" flex ">
+    <div className="px-8">
+      <div className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-6">
+              <div className="card">
+                <div className="card-header border-0">
+                  <div className="d-flex justify-content-between">
+                    <h3 className="card-title">Online Store Visitors</h3>
+                    <a href="javascript:void(0);">View Report</a>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="d-flex">
+                    <p className="d-flex flex-column">
+                      <span className="text-bold text-lg">820</span>
+                      <span>Visitors Over Time</span>
+                    </p>
+                    <p className="ml-auto d-flex flex-column text-right">
+                      <span className="text-success">
+                        <i className="fas fa-arrow-up" /> 12.5%
+                      </span>
+                      <span className="text-muted">Since last week</span>
+                    </p>
+                  </div>
+                  {/* /.d-flex */}
+                  <div className="position-relative mb-4">
+                    <canvas id="visitors-chart" height={200} />
+                  </div>
+                  <div className="d-flex flex-row justify-content-end">
+                    <span className="mr-2">
+                      <i className="fas fa-square text-primary" /> This Week
+                    </span>
+                    <span>
+                      <i className="fas fa-square text-gray" /> Last Week
+                    </span>
+                  </div>
+                </div>
+              </div>
+              {/* /.card */}
+              <div className="card">
+                <div className="card-header border-0">
+                  <h3 className="card-title">Products</h3>
+                  <div className="card-tools">
+                    <a href="#" className="btn btn-tool btn-sm">
+                      <i className="fas fa-download" />
+                    </a>
+                    <a href="#" className="btn btn-tool btn-sm">
+                      <i className="fas fa-bars" />
+                    </a>
+                  </div>
+                </div>
+                <div className="card-body table-responsive p-0">
+                  <table className="table table-striped table-valign-middle">
+                    <thead>
+                      <tr>
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Sales</th>
+                        <th>More</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <img
+                            src="dist/img/default-150x150.png"
+                            alt="Product 1"
+                            className="img-circle img-size-32 mr-2 inline"
+                          />
+                          Some Product
+                        </td>
+                        <td>$13 USD</td>
+                        <td>
+                          <small className="text-success mr-1">
+                            <i className="fas fa-arrow-up" />
+                            12%
+                          </small>
+                          12,000 Sold
+                        </td>
+                        <td>
+                          <a href="#" className="text-muted">
+                            <i className="fas fa-search" />
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="dist/img/default-150x150.png"
+                            alt="Product 1"
+                            className="img-circle img-size-32 mr-2 inline"
+                          />
+                          Another Product
+                        </td>
+                        <td>$29 USD</td>
+                        <td>
+                          <small className="text-warning mr-1">
+                            <i className="fas fa-arrow-down" />
+                            0.5%
+                          </small>
+                          123,234 Sold
+                        </td>
+                        <td>
+                          <a href="#" className="text-muted">
+                            <i className="fas fa-search" />
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="dist/img/default-150x150.png"
+                            alt="Product 1"
+                            className="img-circle img-size-32 mr-2 inline "
+                          />
+                          Amazing Product
+                        </td>
+                        <td>$1,230 USD</td>
+                        <td>
+                          <small className="text-danger mr-1">
+                            <i className="fas fa-arrow-down" />
+                            3%
+                          </small>
+                          198 Sold
+                        </td>
+                        <td>
+                          <a href="#" className="text-muted">
+                            <i className="fas fa-search" />
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <img
+                            src="dist/img/default-150x150.png"
+                            alt="Product 1"
+                            className="img-circle img-size-32 mr-2 inline"
+                          />
+                          Perfect Item
+                          <span className="badge bg-danger">NEW</span>
+                        </td>
+                        <td>$199 USD</td>
+                        <td>
+                          <small className="text-success mr-1">
+                            <i className="fas fa-arrow-up" />
+                            63%
+                          </small>
+                          87 Sold
+                        </td>
+                        <td>
+                          <a href="#" className="text-muted">
+                            <i className="fas fa-search" />
+                          </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              {/* /.card */}
+            </div>
+            {/* /.col-md-6 */}
+            <div className="col-lg-6">
+              <div className="card">
+                <div className="card-header border-0">
+                  <div className="d-flex justify-content-between">
+                    <h3 className="card-title">Sales</h3>
+                    <a href="javascript:void(0);">View Report</a>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="d-flex">
+                    <p className="d-flex flex-column">
+                      <span className="text-bold text-lg">$18,230.00</span>
+                      <span>Sales Over Time</span>
+                    </p>
+                    <p className="ml-auto d-flex flex-column text-right">
+                      <span className="text-success">
+                        <i className="fas fa-arrow-up" /> 33.1%
+                      </span>
+                      <span className="text-muted">Since last month</span>
+                    </p>
+                  </div>
+                  {/* /.d-flex */}
+                  <div className="position-relative mb-4">
+                    <canvas id="sales-chart" height={200} />
+                  </div>
+                  <div className="d-flex flex-row justify-content-end">
+                    <span className="mr-2">
+                      <i className="fas fa-square text-primary" /> This year
+                    </span>
+                    <span>
+                      <i className="fas fa-square text-gray" /> Last year
+                    </span>
+                  </div>
+                </div>
+              </div>
+              {/* /.card */}
+              <div className="card">
+                <div className="card-header border-0">
+                  <h3 className="card-title">Online Store Overview</h3>
+                  <div className="card-tools">
+                    <a href="#" className="btn btn-sm btn-tool">
+                      <i className="fas fa-download" />
+                    </a>
+                    <a href="#" className="btn btn-sm btn-tool">
+                      <i className="fas fa-bars" />
+                    </a>
+                  </div>
+                </div>
+                <div className="card-body">
+                  <div className="d-flex justify-content-between align-items-center border-bottom mb-3">
+                    <p className="text-success text-xl">
+                      <i className="ion ion-ios-refresh-empty" />
+                    </p>
+                    <p className="d-flex flex-column text-right">
+                      <span className="font-weight-bold">
+                        <i className="ion ion-android-arrow-up text-success" />{" "}
+                        12%
+                      </span>
+                      <span className="text-muted">CONVERSION RATE</span>
+                    </p>
+                  </div>
+                  {/* /.d-flex */}
+                  <div className="d-flex justify-content-between align-items-center border-bottom mb-3">
+                    <p className="text-warning text-xl">
+                      <i className="ion ion-ios-cart-outline" />
+                    </p>
+                    <p className="d-flex flex-column text-right">
+                      <span className="font-weight-bold">
+                        <i className="ion ion-android-arrow-up text-warning" />{" "}
+                        0.8%
+                      </span>
+                      <span className="text-muted">SALES RATE</span>
+                    </p>
+                  </div>
+                  {/* /.d-flex */}
+                  <div className="d-flex justify-content-between align-items-center mb-0">
+                    <p className="text-danger text-xl">
+                      <i className="ion ion-ios-people-outline" />
+                    </p>
+                    <p className="d-flex flex-column text-right">
+                      <span className="font-weight-bold">
+                        <i className="ion ion-android-arrow-down text-danger" />{" "}
+                        1%
+                      </span>
+                      <span className="text-muted">REGISTRATION RATE</span>
+                    </p>
+                  </div>
+                  {/* /.d-flex */}
+                </div>
+              </div>
+            </div>
+            {/* /.col-md-6 */}
+          </div>
+          {/* /.row */}
+        </div>
+        {/* /.container-fluid */}
+      </div>
+
       <div>
-        <Doughnut data={data} />
+        <div className="row">
+          <div className="col-lg-3 col-6">
+            {/* small box */}
+            <div className="small-box bg-info">
+              <div className="inner">
+                <h3>150</h3>
+                <p>New Orders</p>
+              </div>
+              <div className="icon">
+                <i className="ion ion-bag" />
+              </div>
+              <a href="#" className="small-box-footer">
+                More info <i className="fas fa-arrow-circle-right" />
+              </a>
+            </div>
+          </div>
+          {/* ./col */}
+          <div className="col-lg-3 col-6">
+            {/* small box */}
+            <div className="small-box bg-success">
+              <div className="inner">
+                <h3>
+                  53<sup style={{ fontSize: 20 }}>%</sup>
+                </h3>
+                <p>Bounce Rate</p>
+              </div>
+              <div className="icon">
+                <i className="ion ion-stats-bars" />
+              </div>
+              <a href="#" className="small-box-footer">
+                More info <i className="fas fa-arrow-circle-right" />
+              </a>
+            </div>
+          </div>
+          {/* ./col */}
+          <div className="col-lg-3 col-6">
+            {/* small box */}
+            <div className="small-box bg-warning">
+              <div className="inner">
+                <h3>44</h3>
+                <p>User Registrations</p>
+              </div>
+              <div className="icon">
+                <i className="ion ion-person-add" />
+              </div>
+              <a href="#" className="small-box-footer">
+                More info <i className="fas fa-arrow-circle-right" />
+              </a>
+            </div>
+          </div>
+          {/* ./col */}
+          <div className="col-lg-3 col-6">
+            {/* small box */}
+            <div className="small-box bg-danger">
+              <div className="inner">
+                <h3>65</h3>
+                <p>Unique Visitors</p>
+              </div>
+              <div className="icon">
+                <i className="ion ion-pie-graph" />
+              </div>
+              <a href="#" className="small-box-footer">
+                More info <i className="fas fa-arrow-circle-right" />
+              </a>
+            </div>
+          </div>
+          {/* ./col */}
+        </div>
       </div>
-      <div className=" w-[500px]">
-        <PieChart/>
-      </div>
-      
+
     </div>
   );
 };
