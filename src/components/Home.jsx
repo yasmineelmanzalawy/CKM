ef2275e15469ebbf44b0760a0301325df35bfa9c
BuildTools <unconfigured@null.spigotmc.org>
2023-06-25 11:26:57 +0300

    fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -164,7 +164,10 @@ function Home() {
                 CKM Helps You To Manage..
               </h5>
               <p className="font-normal text-gray-700">
-              Unlock the potential of your cloud kitchen business with CKM's comprehensive suite of tools, empowering you to deliver exceptional customer experiences while simplifying your day-to-day management tasks.
+                Unlock the potential of your cloud kitchen business with CKM's
+                comprehensive suite of tools, empowering you to deliver
+                exceptional customer experiences while simplifying your
+                day-to-day management tasks.
               </p>
             </div>
           </div>
@@ -287,7 +290,7 @@ function Home() {
                       ></path>
                     </svg>
                     <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-                      basic inventory tracking
+                      Staff Management
                     </span>
                   </li>
                   <li class="flex space-x-3 my-4">
@@ -346,7 +349,7 @@ function Home() {
                       ></path>
                     </svg>
                     <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-                    basic inventory tracking
+                      menu customization
                     </span>
                   </li>
                   <li class="flex space-x-3 my-4">
@@ -365,7 +368,7 @@ function Home() {
                       ></path>
                     </svg>
                     <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-                    menu customization
+                      order processing
                     </span>
                   </li>
                   <li class="flex space-x-3 my-4">
@@ -384,26 +387,7 @@ function Home() {
                       ></path>
                     </svg>
                     <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-                    order processing
-                    </span>
-                  </li>
-                  <li class="flex space-x-3 my-4">
-                    <svg
-                      aria-hidden="true"
-                      class="flex-shrink-0 w-5 h-5 text-blue-600 dark:text-blue-500"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <title>Check icon</title>
-                      <path
-                        fill-rule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                    <span class="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-                    Essential features for effective cloud kitchen management
+                      Essential features for effective cloud kitchen management
                     </span>
                   </li>
                 </div>
